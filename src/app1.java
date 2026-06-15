25d0857b7861ba3032de4eab0a99db7ca396e339
Kaio Henrique <kaiohenriquemalaquias@gmail.com>
2026-06-15 13:15:55 -0300

    .

--- src/app1.java
+++ src/app1.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class app1 {
-    
+
+    public static void main(String [] args) {
+        try (var scanner = new Scanner(System.in)) {
+            System.out.println("Quantos anos vc tem?");
+            var age = scanner.nextInt();
+            System.out.println("Vc e emancipado?");
+            var IsEmancipated = scanner.nextBoolean();
+            var liberado = age >= 18 || (IsEmancipated && age >= 16);
+            System.out.printf("Vc pode dirigir? %b%n", liberado);
+        }
+    }
 }
