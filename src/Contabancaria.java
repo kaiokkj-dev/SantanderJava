d0e3115eb6a0d486f5b4862d58553d1080351bde
Kaio Henrique <kaiohenriquemalaquias@gmail.com>
2026-07-09 16:32:41 -0300

    Studying.

--- src/Contabancaria.java
+++ src/Contabancaria.java
@@ -1,37 +1,47 @@
-
-import javax.lang.model.util.ElementScanner14;
-
 public class ContaBancaria {
     private double saldo;
-    private double chequeEspecial;
-    private double chequeEspeciaUsado;
-
-        public ContaBancaria(double depositoInicial) {
+    private final double chequeEspecial;
+    private double chequeEspecialUsado;
+    public ContaBancaria(double depositoInicial) {
         this.saldo = depositoInicial;
-        if(depositoInicial <= 500) {
+        if (depositoInicial <= 500) {
             this.chequeEspecial = 50;
         } else {
             this.chequeEspecial = depositoInicial * 0.5;
         }
     }
     public double consultarSaldo() {
-    return saldo;
-}
-    public double consultarChequeEspecial() {
-    return chequeEspecial;
-}
-public void depositar(double valor) {
-    saldo =+ valor;
-}
-public void sacar(double valor) {
-    if (valor <= saldo) {
-        saldo -= valor;
-    } else if (valor <= saldo + chequeEspecial) {
-        double usado = valor - saldo;
-        saldo = 0;
-        chequeEspecial = usado;
-    } else {
-        System.out.println("Saldo insuficiente!");
+        return saldo;
     }
-}
+    public double consultarChequeEspecial() {
+        return chequeEspecial;
+    }
+    public void depositar(double valor) {
+        saldo += valor;
+        if (chequeEspecial > 0) {
+            double taxa = chequeEspecialUsado * 0.20;
+            double divida = chequeEspecialUsado + taxa;
+            if (saldo >= divida) {
+                saldo -= divida;
+                chequeEspecialUsado = 0;
+            }
+        }
+    }
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+        } else if (valor <= saldo + (chequeEspecial - chequeEspecialUsado)) {
+            double usado = valor - saldo;
+            saldo = 0;
+            chequeEspecialUsado += usado;
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+    public void pagarBoleto(double valor) {
+        sacar(valor);
+    }
+    public boolean verificarChequeEspecial() {
+        return chequeEspecialUsado > 0;
+    }
 }
---
+++ src/Minhaconta.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Minhaconta {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o deposito inicial: ");
+        double depositoInicial = sc.nextDouble();
+        ContaBancaria conta = new ContaBancaria(depositoInicial);
+        int opcao = 0;
+        while (opcao != 6) {
+            System.out.println("\n===== BANCO =====");
+            System.out.println("1 - Consultar saldo");
+            System.out.println("2 - Depositar");
+            System.out.println("3 - Sacar");
+            System.out.println("4 - Pagar boleto");
+            System.out.println("5 - Verificar cheque especial");
+            System.out.println("6 - Sair");
+            System.out.print("Escolha uma opcao: ");
+            opcao = sc.nextInt();
+            if (opcao == 1) {
+                System.out.println("Saldo: " + conta.consultarSaldo());
+            } else if (opcao == 2) {
+                System.out.print("Digite o valor do deposito: ");
+                double valor = sc.nextDouble();
+                conta.depositar(valor);
+                System.out.println("Deposito realizado.");
+            } else if (opcao == 3) {
+                System.out.print("Digite o valor do saque: ");
+                double valor = sc.nextDouble();
+                conta.sacar(valor);
+            } else if (opcao == 4) {
+                System.out.print("Digite o valor do boleto: ");
+                double valor = sc.nextDouble();
+                conta.pagarBoleto(valor);
+            } else if (opcao == 5) {
+                System.out.println("Usando cheque especial: " + conta.verificarChequeEspecial());
+            } else if (opcao == 6) {
+                System.out.println("Saindo...");
+            } else {
+                System.out.println("Opcao invalida.");
+            }
+        }
+        sc.close();
+    }
+}
